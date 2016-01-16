aef92688f09b20d6cada11ff59d49825db7c84ce
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-01-17 00:32:07 +0200

    Fix dependencies in project.clj. Proper support for 3003 when launching ring server through -main.

--- src/pigeon_backend/handler.clj
+++ src/pigeon_backend/handler.clj
@@ -3,7 +3,8 @@
             [ring.util.http-response :refer :all]
             [schema.core :as s]
             [pigeon-backend.routes.hello :refer [hello-routes]]
-            [ring.server.standalone :as ring])
+            [ring.server.standalone :as ring]
+            [environ.core :refer [env]])
   (:gen-class))
 
 (defapi app
@@ -16,5 +17,13 @@
     :tags ["hello"]
     hello-routes))
 
+(defn handle-port-env [v]
+  (cond (and (string? v)
+             ((complement empty?) v))
+        (Integer/parseInt (env :port))
+        (integer? v)
+        v))
+
 (defn -main [& args]
-  (ring/serve app {:port 3000}))
+  (let [port (handle-port-env (env :port))]
+    (ring/serve app {:port port})))
