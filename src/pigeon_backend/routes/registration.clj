7e694ed47fa3274abebee78c1edfedbf8848ffb9
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-04-06 00:49:10 +0300

    HTTP status code: 200 -> 201

--- src/pigeon_backend/routes/registration.clj
+++ src/pigeon_backend/routes/registration.clj
@@ -1,6 +1,7 @@
 (ns pigeon-backend.routes.registration
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
+            [ring.util.http-status :as status]
             [schema.core :as s]))
 
 (def registration-routes
@@ -10,4 +11,4 @@
     (PUT "/" []
       :body-params [username :- String, password :- String]
       :summary "Creates a user account with given unique username and password"
-      (ok))))
+      (created))))
