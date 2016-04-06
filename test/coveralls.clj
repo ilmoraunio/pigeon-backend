a0275c76f07ce9b43a28e3b915cddc9a4e3ac711
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-04-07 00:20:14 +0300

    Prepare coveralls.json with repo token

---
+++ test/coveralls.clj
@@ -0,0 +1,16 @@
+(ns coveralls
+  (:require [cheshire.core :as cheshire]
+            [clojure.java.io :as io]
+            [environ.core :refer [env]]))
+
+; lein run -m coveralls to add relevant repo token
+
+(defn -main [& args]
+  (let [json-as-object (cheshire/parse-string 
+                         (slurp "cov/coveralls.json") 
+                         true)
+        repo-token     (env :coveralls-repo-token)
+        json-with-repo-token (merge json-as-object {:repo_token repo-token})
+        json-encoded (cheshire/encode json-with-repo-token)]
+    (with-open [wrtr (io/writer "cov/coveralls.json")]
+      (.write wrtr json-encoded))))
