41bcf6a18ca1178174bfb2bb2dad584c85d5e5c9
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-04-06 23:14:42 +0300

    Fix cloverage command in coveralls.sh

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,3 +1,3 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.7-SNAPSHOT' lein cloverage -o cov --coveralls
+CLOVERAGE_VERSION='1.0.7-SNAPSHOT' lein with-profile +test cloverage -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
