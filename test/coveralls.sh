16446d655d533b0abefcd36ce67edf9292602eef
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-04-07 00:22:57 +0300

    Move coveralls.clj call to shell script

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,3 +1,4 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 CLOVERAGE_VERSION='1.0.7-SNAPSHOT' lein with-profile +test cloverage -o cov --coveralls
+lein run -m coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
