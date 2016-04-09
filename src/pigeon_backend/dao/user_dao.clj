c7da138037cffcd1280639de107dca2cbac59642
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-04-09 13:51:24 +0300

    Added exception handling to user dao insert.

--- src/pigeon_backend/dao/user_dao.clj
+++ src/pigeon_backend/dao/user_dao.clj
@@ -3,7 +3,8 @@
             [yesql.core :refer [defquery]]
             [pigeon-backend.db.config :refer [db-spec]]
             [clojure.java.jdbc :as jdbc])
-  (import org.postgresql.util.PSQLException))
+  (import org.postgresql.util.PSQLException)
+  (import java.sql.BatchUpdateException))
 
 (s/defschema UserModel {:id s/Int 
                         :username String 
@@ -27,9 +28,18 @@
 
 (defn create! [user] {:pre [(s/validate NewUser user)]
                      :post [(true? %)]}
-  (jdbc/with-db-transaction [tx db-spec]
-    (sql-user-create! user {:connection tx})
-    true))
+  (try
+    (jdbc/with-db-transaction [tx db-spec]
+      (sql-user-create! user {:connection tx})
+      true)
+    (catch BatchUpdateException e
+      (let [message (-> e .getNextException .getMessage)
+            duplicate-user #"duplicate key value violates unique constraint"]
+        (when-let [[_ field] (re-find duplicate-user message)]
+          (throw
+            (ex-info
+              (format "Duplicate user %s found" (:username user))
+              user)))))))
 
 (defn get-from-db [])
 
