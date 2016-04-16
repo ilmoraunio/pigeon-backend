4349a931b1cdb18cead7bb7fbc66b867d9a3692a
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-04-16 22:50:14 +0300

    Remove redundant hello-test.

--- test/pigeon_backend/routes/hello_test.clj
+++ test/pigeon_backend/routes/hello_test.clj
@@ -1,21 +1,28 @@
 (ns pigeon-backend.routes.hello-test
   (:require [clojure.test :refer [deftest]]
+            [cheshire.core :as cheshire]
             [midje.sweet :refer :all]
             [pigeon-backend.handler :refer :all]
-            [ring.mock.request :as mock]
-            [clojure.data.json :as json]
-            [pigeon-backend.dao.user-dao :refer [sql-user-get-all]]
-            [pigeon-backend.test-util :refer [drop-and-create-tables
-                                              parse-body]]))
+            [ring.mock.request :as mock]))
+
+(defn parse-body [body]
+  (cheshire/parse-string (slurp body) true))
+
+(def test-token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJ1c2VyIjoiZm9vYmFyIiwicm9sZXMiOlsiYXBwLWZyb250cGFnZSJdfQ.pAxX-x7zT_deUOpqi2hCmZySYMtwa-yGlocDhH_alKc")
 
 (deftest hello-test
-  (facts "Route: hello"
-    (with-state-changes [(before :facts (drop-and-create-tables))]
-      (fact "Unauthorized test (WIP)"
-        (let [{status :status body :body} 
-                ((app-with-middleware)
-                  (mock/request :get "/hello?name=foo"))]
-          status => 401
-          (parse-body body) => {:cause "signature"
-                                :error-status 401
-                                :title "Not logged in"})))))
+  (facts "Testing /hello"
+    (fact "Test GET request to /hello?name={a-name} returns expected response"
+      (let [response (app (-> (assoc-in (mock/request :get "/hello?name=Stranger")
+                                        [:cookies "token" :value]
+                                        test-token)))
+            body     (parse-body (:body response))]
+        (:status response) => 200
+        (:message body)    => "Terve, Stranger"))
+    (fact "Test GET request to /hello/en?name={a-name} returns expected response"
+      (let [response (app (-> (assoc-in (mock/request :get "/hello/en?name=Stranger")
+                                        [:cookies "token" :value]
+                                        test-token)))
+            body     (parse-body (:body response))]
+        (:status response) => 200
+        (:message body)    => "Hello, Stranger"))))
