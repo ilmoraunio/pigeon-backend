4661332b9af2f680015a186218ebfb9ece17cc66
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-05-08 12:23:25 +0300

    Add missing groupuser dao & test

---
+++ test/pigeon_backend/dao/groupuser_dao_test.clj
@@ -0,0 +1,43 @@
+(ns pigeon-backend.dao.groupuser-dao-test
+  (:require [clojure.test :refer [deftest]]
+            [midje.sweet :refer :all]
+            [pigeon-backend.dao.room-dao :as room-dao]
+            [pigeon-backend.dao.roomgroup-dao :as roomgroup-dao]
+            [pigeon-backend.dao.user-dao :as user-dao]
+            [pigeon-backend.dao.room-dao-test :refer [room-dto]]
+            [pigeon-backend.dao.roomgroup-dao-test :refer [roomgroup-dto]]
+            [pigeon-backend.dao.user-dao-test :refer [user-dto]]
+            [pigeon-backend.dao.groupuser-dao :as dao]
+            [schema.core :as s]
+            [pigeon-backend.db.config :refer [db-spec]]
+            [pigeon-backend.test-util :refer [empty-and-create-tables]]))
+
+(defn groupuser-dto [roomgroup_id userid]
+  {:roomgroup_id roomgroup_id
+   :users_id userid})
+
+(defn groupuser-expected [userid]
+  (contains {:roomgroup_id "Pigeon room"}
+            {:users_id userid}
+            {:created #(instance? java.util.Date %)}
+            {:updated #(instance? java.util.Date %)}
+            {:version 0}
+            {:deleted false}))
+
+(deftest groupuser-dao-test
+  (facts "Dao: groupuser create"
+    (with-state-changes [(before :facts (empty-and-create-tables))]
+      (fact "Basic case"
+        (let [{userid :id} (user-dao/create! db-spec user-dto)
+              _ (room-dao/create! db-spec room-dto)
+              {roomgroup_id :id} (roomgroup-dao/create! db-spec roomgroup-dto)]
+          (dao/create! db-spec (groupuser-dto roomgroup_id userid))
+            => groupuser-expected))
+      (fact "Duplicate groupuser inside room not allowed"
+        (let [{userid :id} (user-dao/create! db-spec user-dto)
+              _ (room-dao/create! db-spec room-dto)
+              {roomgroup_id :id} (roomgroup-dao/create! db-spec roomgroup-dto)]
+          (dao/create! db-spec (groupuser-dto roomgroup_id userid))
+          (dao/create! db-spec (groupuser-dto roomgroup_id userid))
+            => (throws clojure.lang.ExceptionInfo
+                "Duplicate group user"))))))
