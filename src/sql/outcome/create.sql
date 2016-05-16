20f356e97007c0848f92f788e9653e9557ab649c
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-05-16 21:48:19 +0300

    Add outcome dao + tests

--- src/sql/outcome/create.sql
+++ src/sql/outcome/create.sql
@@ -1,2 +1,2 @@
 INSERT INTO Outcome (Property_id, type, value)
-VALUES ((:propertyid)::integer, (:type)::varchar(1000), (:value)::varchar(1000));
+VALUES ((:property_id)::integer, (:type)::varchar(1000), (:value)::varchar(1000));
---
+++ test/pigeon_backend/dao/outcome_dao_test.clj
@@ -0,0 +1,35 @@
+(ns pigeon-backend.dao.outcome-dao-test
+  (:require [clojure.test :refer [deftest]]
+            [midje.sweet :refer :all]
+            [pigeon-backend.dao.outcome-dao :as dao]
+            [pigeon-backend.dao.dao-mother :as dao-mother]
+            [schema.core :as s]
+            [pigeon-backend.db.config :refer [db-spec]]
+            [pigeon-backend.test-util :refer [empty-and-create-tables]]))
+
+(defn outcome-data [property-id]
+  {:property_id property-id
+   :type "outcome type"
+   :value "outcome value"})
+
+(defn outcome-data-expected [property-id]
+  (contains {:id integer?}
+            {:property_id property-id}
+            {:type "outcome type"}
+            {:value "outcome value"}
+            {:created #(instance? java.util.Date %)}
+            {:updated #(instance? java.util.Date %)}
+            {:version 0}
+            {:deleted false}))
+
+(deftest outcome-dao-test
+  (facts "Dao: outcome create"
+    (with-state-changes [(before :facts (empty-and-create-tables))]
+      (fact "Basic case"
+        (let [property (dao-mother/property)]
+          (dao/create! db-spec (outcome-data (:id property)))
+            => (outcome-data-expected (:id property))))
+      (fact "Allow duplicates"
+        (let [property (dao-mother/property)]
+          (dao/create! db-spec (outcome-data (:id property)))
+            => (outcome-data-expected (:id property)))))))
