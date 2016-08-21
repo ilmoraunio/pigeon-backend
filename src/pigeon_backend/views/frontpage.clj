3b08d3e3162dfe66af4621af2c9cac45bb3cb640
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-08-21 22:48:12 +0300

    Revert web route changes

--- src/pigeon_backend/views/frontpage.clj
+++
@@ -1,9 +0,0 @@
-(ns pigeon-backend.views.frontpage
-  (:require [hiccup.page :as h]
-            [pigeon-backend.views.layout :as layout]))
-
-(defn index []
-  (layout/common 
-    "frontpage"
-    [:div [:p [:a {:href "/Registration"} "Sign up"]]
-          [:p [:a {:href "/login"} "Login"]]]))
