d5299d452560c04673ea51bbad72e8da9b8259e1
Ilmo Raunio <ilmo.raunio@gmail.com>
2016-09-13 20:16:07 +0300

    Disable authorization with options requests (#57)
    * Disable authorization with OPTIONS method requests
    * Add allowable headers to Access-Control-Allow-Headers

--- src/pigeon_backend/middleware.clj
+++ src/pigeon_backend/middleware.clj
@@ -3,24 +3,28 @@
            [pigeon-backend.services.exception-util :refer [handle-exception-info]]
            [environ.core :refer [env]]))
 
+(def ^{:private true} authorizable-http-methods #{:get :head :post :put :delete})
+
 (defn wrap-auth [handler]
   (fn [request]
     (let [headers (:headers request)]
-
-      (if-not (contains? headers "authorization")
-        (handle-exception-info
-          (ex-info "Not logged in" {:type :validation
-                                    :cause :signature}) {} request))
-
-      (if (nil? (get headers "authorization"))
-        (handle-exception-info
-          (ex-info "Not logged in" {:type :validation
-                                    :cause :signature}) {} request)
-        (let [token (second (re-find #"Bearer (.*?)$" (get headers "authorization")))]
-          (if (or (empty? token) (< (count token) 3))
+      (if-let [should-request-be-authorized? ((:request-method request) authorizable-http-methods)]
+        (do
+          (if-not (contains? headers "authorization")
+            (handle-exception-info
+              (ex-info "Not logged in" {:type :validation
+                                        :cause :signature}) {} request))
+          (if (nil? (get headers "authorization"))
             (handle-exception-info
               (ex-info "Not logged in" {:type :validation
                                         :cause :signature}) {} request)
-            (do
-              (jws/unsign token (env :jws-shared-secret))
-              (handler request))))))))
+            (let [token (second (re-find #"Bearer (.*?)$" (get headers "authorization")))]
+              (if (or (empty? token) (< (count token) 3))
+                (handle-exception-info
+                  (ex-info "Not logged in" {:type :validation
+                                            :cause :signature}) {} request)
+                (do
+                  (jws/unsign token (env :jws-shared-secret))
+                  (handler request))))))
+
+        (handler request)))))
