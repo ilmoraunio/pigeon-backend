4886d1ae733c00feca22ee313278d0fe5f352650
Ilmo Raunio <ilmo.raunio@gmail.com>
2017-06-27 23:17:12 +0300

    Add message.sql + some adjustments to ddl

---
+++ src/sql/message.sql
@@ -0,0 +1,33 @@
+-- name: sql-message-create<!
+INSERT INTO message (sender,
+                     recipient,
+                     actual_recipient,
+                     message,
+                     turn)
+     VALUES ((:sender)::varchar(255),
+             (:recipient)::varchar(255),
+             (:actual_recipient)::varchar(255),
+             (:message)::text,
+             (:turn)::integer);
+
+-- name: sql-message-get
+      SELECT message.sender,
+             message.recipient,
+             message.actual_recipient,
+             message.message,
+             message.turn,
+             message.created,
+             message.updated,
+             message.version,
+             message.deleted,
+             turn.name
+        FROM message
+  INNER JOIN turn
+          ON turn.id = message.turn
+       WHERE ((message.sender = (:sender)::varchar(255)
+               AND message.actual_recipient = (:recipient)::varchar(255))
+           OR (message.sender = (:recipient)::varchar(255)
+               AND message.actual_recipient = (:sender)::varchar(255)))
+         AND message.deleted = false
+    ORDER BY turn.ordering ASC,
+             message.created ASC;
