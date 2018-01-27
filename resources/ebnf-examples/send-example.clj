b93e5ca2de14fa644d39b0d9dc64f5a541834d46
Ilmo Raunio <ilmo.raunio@gmail.com>
2018-01-27 13:17:46 +0200

    Update send example

--- resources/ebnf-examples/send-example.clj
+++ resources/ebnf-examples/send-example.clj
@@ -1,2 +1,10 @@
-(let [params *params*]
-  (send (:message params) (:intended-recipients params)))
+(let [tx (:tx *params*)
+      message (:message *params*)
+      sender (:sender *params*)
+      message-attempt-id (:message_attempt *params*)
+      recipient (:recipient *params*)]
+  (send-message tx {:message message
+                    :sender sender
+                    :message_attempt message-attempt-id
+                    :recipient recipient
+                    :actual_recipient recipient}))
